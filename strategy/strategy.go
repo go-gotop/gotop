99c5ebb524d81b316c7681a2b94e1e2e29f5e6ee
hom <hcclinux@gmail.com>
2024-12-12 21:40:49 +0800

    chore: Strategy新增SetStatus方法

--- strategy/strategy.go
+++ strategy/strategy.go
@@ -1,9 +1,21 @@
 package strategy
 
+import (
+	"context"
+
+	"github.com/go-gotop/gotop/types"
+)
+
 // Strategy 策略接口
-type Strategy[T any, S any] interface {
+type Strategy[Event any, NotifyData any, Result any] interface {
+	// SetStatus 设置策略状态
+	SetStatus(status types.StrategyStatus)
+	// Status 返回策略状态
+	Status() types.StrategyStatus
+	// Notify 通知策略信号变化
+	Notify(ctx context.Context, notifyData NotifyData)
 	// Next 处理事件, 返回处理结果
-	Next(event T) S
-	// Name 策略名称
-	Name() string
+	Next(ctx context.Context, event Event) Result
+	// ID 策略ID
+	ID() string
 }
